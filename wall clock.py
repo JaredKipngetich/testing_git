aa185d890574a3e416d8b116fab72275b884bcc5
JaredKipngetich <119751579+JaredKipngetich@users.noreply.github.com>
2023-02-27 22:15:03 +0300

    Add files via upload

---
+++ wall clock.py
@@ -0,0 +1,13 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+import turtle
+import random
+
+for x in range(0,10):
+    for i in range(0,8):
+        turtle.forward(50)
+        turtle.right(45)
+        turtle.right(36)
+        turtle.hideturtle()
+
+        
