ac4996fe19763f09cc4b0a8200106adfb168da5c
JaredKipngetich <119751579+JaredKipngetich@users.noreply.github.com>
2023-02-28 22:25:11 +0300

    Add files via upload

---
+++ triangle.py
@@ -0,0 +1,23 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+import turtle
+for i in range(0,3):
+    turtle.right(60)
+
+    
+for i in range(0,3):
+    turtle.forward(100)
+    turtle.left(120)
+
+    
+Traceback (most recent call last):
+  File "<pyshell#7>", line 2, in <module>
+    turtle.forward(100)
+  File "<string>", line 5, in forward
+turtle.Terminator
+import turtle
+for i in range(0,3):
+    turtle.forward(100)
+    turtle.left(120)
+
+    
